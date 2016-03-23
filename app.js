ec441b8cddfc9789b927e760253a6384ae4b361f
goldfish819 <goldfish819@hanmail.net>
2016-03-23 16:34:56 +0900

    chatt

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.createServer(app);
+var http = require('http').Server(app);
+// var server = http.createServer(app);
 // var fs = require('fs');
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(http);
+var localStorage = require('localStorage');
+var sessionStorage = require('sessionstorage');
 
 
 app.use('/public', express.static('public'))
@@ -41,20 +43,58 @@ io.on('connection',function (socket) {
     });
 });
 
+//create
+var socketRoom = {};
+var roomCount =1;
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
+    socket.on('create group', function () {
+        console.log('num: '+roomCount+'생성');
+        socketRoom[roomCount] = socket.id;
+        console.log(socketRoom);
+        io.emit('create group',roomCount);
+        roomCount++;
+        var rooms = io.sockets.adapter;
+        console.log(rooms);
+    });
+});
+
+//join
+io.on('connection',function (socket) {
+    socket.on('join group', function (num) {
+        //if(localStorage.getItem('roomNum') === null){
+        if(sessionStorage.length == 0){
+            socket.join(num);
+            console.log(num+'번방');
+            //localStorage.setItem('roomNum',num);
+            sessionStorage.setItem('roomNum',num);
+            
+        }else{
+            // socket.leave(localStorage.getItem('roomNum'));
+            // console.log('leave: '+localStorage.getItem('roomNum'));
+            socket.leave(sessionStorage.getItem('roomNum'));
+            console.log('leave: '+sessionStorage.getItem('roomNum'));
+            socket.join(num);
+            console.log(num+'번방');
+            //localStorage.setItem('roomNum',num);
+            sessionStorage.setItem('roomNum',num);
+        }
+    });
+});
+
+//message
+io.on('connection', function (socket) {
+    socket.on('chat message', function (name,msg) {
         // console.log('message:'+ msg);
-        io.emit('chat message', msg);
+        // io.emit('chat message', msg);
+        // io.sockets.in(localStorage.getItem('roomNum')).emit('chat message', name, msg);
+        // console.log('roomnum: '+localStorage.getItem('roomNum')+' /text: '+msg);
+        io.sockets.in(sessionStorage.getItem('roomNum')).emit('chat message', name, msg);
+        console.log('roomnum: '+sessionStorage.getItem('roomNum')+' /text: '+msg);
     });
 });
 
-io.on('connection', function (socket) {
-    socket.on('create group', function (div) {
-        io.emit('create group', div);
-    });
-});
 
 
-server.listen(3000, function() {
+http.listen(3000, function() {
 	console.log('listening on: 3000');
 })
